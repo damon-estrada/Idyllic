53b76debfb0c07802e23690fb9b038382028695a
damon_estrada <damon.estrada@cs.nmt.edu>
2019-04-24 22:55:53 -0600

    Cleaned up MainActivity. Did not need to use AppRemote since this app does not need it.

--- app/src/main/java/com/example/moodvisulized/MainActivity.java
+++ app/src/main/java/com/example/moodvisulized/MainActivity.java
@@ -44,11 +44,6 @@ public class MainActivity extends AppCompatActivity implements Item {
     private static final String CLIENT_ID = "0184658057ca400693856a596026419b";
     private static final String REDIRECT_URI = "moodvisualized://callback";
 
-    // Request code will be used to verify if result comes from the login activity.
-    // Can be set to any integer.
-
-    private SpotifyAppRemote mSpotifyAppRemote;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -73,67 +68,13 @@ public class MainActivity extends AppCompatActivity implements Item {
         startActivity(intent);
     }
 
-
-
     @Override
     protected void onStart() {
         super.onStart();
-/*
-        SpotifyAppRemote.disconnect(mSpotifyAppRemote);
-        // We want to log into the next activity
-        // We need to authorize my application to use the App Remote SDK
-        // If I do not do this, the program will fail with UserNotAuthorizedException
-        // This uses the built-in authorization from the Quick Start Guide
-        ConnectionParams mConnectionParams =
-                new ConnectionParams.Builder(CLIENT_ID)
-                        .setRedirectUri(REDIRECT_URI)
-                        .showAuthView(true)
-                        .build();
-
-        // We need to use SpotifyAppRemote.Connector to get an instance of SpotifyAppRemote
-        // We do this using SpotifyAppRemote.connect using connectionParams.
-        SpotifyAppRemote.connect(this, mConnectionParams,
-                new Connector.ConnectionListener() {
-
-                    @Override
-                    public void onConnected(SpotifyAppRemote spotifyAppRemote) {
-                        mSpotifyAppRemote = spotifyAppRemote;
-                        Log.d("MainActivity", "\n\nConnected! Yay!\n\n");
-
-                        // Now we can start interacting with App Remote
-                        connected();
-                    }
-
-
-                    public void onFailure(Throwable throwable) {
-                        // If we get here, something bad happened, figure out what happened.
-                        Log.e("MainActivity", throwable.getMessage(), throwable);
-                        System.out.println("\n\n\n\nFAILURE TO CONNECT APP REMOTE");
-                    }
-                });
-                */
-    }
-
-    private void connected() {
-        // Once onConnected in onStart() succeeds, we can now start making requests.
-
-        // THIS IS HOW WE GET THE CURRENT TRACK NAME AND BY ARTIST
-        mSpotifyAppRemote.getPlayerApi()
-                .subscribeToPlayerState()
-                .setEventCallback(playerState -> {
-                    final Track track = playerState.track;
-                    if (track != null) {
-                        Log.d("Track: ", track.name + " by " + track.artist.name);
-                        Log.d("URI: ", track.uri);
-                        Log.d("Duration",  Long.toString(track.duration));
-                    }
-                });
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        // We shall disconnect from the App Remote when we do not need it anymore.
-        SpotifyAppRemote.disconnect(mSpotifyAppRemote);
     }
 }
