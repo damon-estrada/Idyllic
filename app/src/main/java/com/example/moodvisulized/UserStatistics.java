2cdc50c54c251e45276889e8ba859601030a42cf
damon_estrada <damon.estrada@cs.nmt.edu>
2019-06-13 03:06:24 -0600

    Removed status bar

--- app/src/main/java/com/example/moodvisulized/UserStatistics.java
+++ app/src/main/java/com/example/moodvisulized/UserStatistics.java
@@ -14,6 +14,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageView;
 
@@ -84,6 +86,10 @@ public class UserStatistics extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // to remove the status bar
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_user_statistics);
 
